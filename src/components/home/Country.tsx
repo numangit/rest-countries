8141c5d12644e194e378b190a5a118faa6c140ac
numangit <abdulnumanjob@gmail.com>
2023-07-14 21:22:48 +0600

    warning removed

--- src/components/home/Country.tsx
+++ src/components/home/Country.tsx
@@ -2,7 +2,7 @@ import { CountryPropsType } from "../../interfaces/interfaces";
 
 
 export default function Country({ country }: CountryPropsType) {
-  const { name, flags, capital, cca2, region } = country;
+  const { name, flags, capital, region } = country;
   return (
     <div className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
       <img
@@ -17,11 +17,14 @@ export default function Country({ country }: CountryPropsType) {
           tabIndex={0}
           role="link"
         >
-          {name.official}
+          {name.common}
         </a>
-        <span className="text-sm text-gray-700 dark:text-gray-200">
-          {capital}
-        </span>
+        <p className="text-sm text-gray-700 text-left ml-3 dark:text-gray-200">
+          Capital: {capital}
+        </p>
+        <p className="text-sm text-gray-700 text-left ml-3 dark:text-gray-200">
+          region: {region}
+        </p>
       </div>
     </div>
   )
